7cff217370f24bc09d7011e35c2993a5f6193481
Naughtron <chris.naughton@gmail.com>
2017-02-18 11:19:48 -0500

    added changes to index.html and views

--- target_site/flask/app/views.py
+++ target_site/flask/app/views.py
@@ -1,17 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    user = {'nickname': 'Naughtron'}  # some user
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['nickname'] + '''</h1>
-  </body>
-</html>
-'''
+    user = {'nickname': 'Naughtron'} # just some user
+    # create array some fake posts
+    posts = [
+        {
+        'author': {'nickname': 'Naughtron'},
+        'body': 'You can\'t fax glitter!'
+        },
+        {
+        'author': {'nickname': 'Some_Dude'},
+        'body': 'well, not with that attitude!'
+        }
+    ]
+    return render_template('index.html',
+                           #title='Home',
+                           user=user,
+                           posts=posts)
