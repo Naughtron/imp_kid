54c054be9e2de82796280bd2605abfbffde3e310
Naughtron <chris.naughton@gmail.com>
2017-02-18 13:29:40 -0500

    adding changes from mod3

--- target_site/flask/app/views.py
+++ target_site/flask/app/views.py
@@ -1,23 +1,36 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+#@app.route('/')
+#@app.route('/index')
+@app.route('/login', methods=['GET', 'POST']) # if not spec only GET works
 
-def index():
-    user = {'nickname': 'Naughtron'} # just some user
-    # create array some fake posts
-    posts = [
-        {
-        'author': {'nickname': 'Naughtron'},
-        'body': 'You can\'t fax glitter!'
-        },
-        {
-        'author': {'nickname': 'Some_Dude'},
-        'body': 'well, not with that attitude!'
-        }
-    ]
-    return render_template('index.html',
-                           #title='Home',
-                           user=user,
-                           posts=posts)
+# def index():
+#     user = {'nickname': 'Naughtron'} # just some user
+#     # create array some fake posts
+#     posts = [
+#         {
+#         'author': {'nickname': 'Naughtron'},
+#         'body': 'You can\'t fax glitter!'
+#         },
+#         {
+#         'author': {'nickname': 'Some_Dude'},
+#         'body': 'well, not with that attitude!'
+#         }
+#     ]
+#     return render_template('index.html',
+#                            #title='Home',
+#                            user=user,
+#                            posts=posts)
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
---
+++ target_site/flask/config.py
@@ -0,0 +1,10 @@
+WTF_CSFR_ENABLED = True # prevents CSS
+SECRET_KEY = "you-will-never-guess" # this is used to create a token
+
+# add openID providers
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
